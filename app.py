d1201c82e829fdc6abf9ea18319542b1b475d288
Yuzu-ponzu <105972291+Yuzu-ponzu@users.noreply.github.com>
2022-06-24 22:43:45 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import csv
+import sinbol
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+object_isSnap=True
+
+class Application(tk.Frame):
+
+    def __init__(self,master=None):
+
+        super().__init__(master)
+        self.master.title("電子連動 進路作成")
+        self.master.geometry("1500x900+200+50")
+        self.master.resizable(width=False, height=False)
+        self.pack()
+
+        self.Route_view=tk.Canvas(master,background="black",height=900,width=1500)
+        self.Route_view.pack(side=tk.LEFT)
+
+        self.isSnap=True
+        self.mode=""
+        self.grid_modo=True
+        self.mouseX,self.mouseY=0,0
+        self.track_name=""
+        self.track_x,self.track_y=[],[]
+        self.point_var=tk.StringVar()
+        Sinbol.Canvas_object.canvas=self.Route_view
+
+        self.point_label=tk.Label(self.Route_view,textvariable=self.point_var,bg="white",font=("HG丸ｺﾞｼｯｸM-PRO",14,"bold"))
+        self.back_line=tk.Button(self.Route_view,text="Track",font=("HG丸ｺﾞｼｯｸM-PRO",14,"bold"),bg="white",command=lambda:self.new_track("None"))
+        self.load_line=tk.Button(self.Route_view,text="Load",font=("HG丸ｺﾞｼｯｸM-PRO",14,"bold"),bg="white")
+        self.clear_list=tk.Button(self.Route_view,text="Clear",font=("HG丸ｺﾞｼｯｸM-PRO",14,"bold"),bg="white")
+        self.open_menu=tk.Button(self.Route_view,text="Menu",font=("HG丸ｺﾞｼｯｸM-PRO",14,"bold"),bg="white")
+        self.line_mode=tk.Button(self.Route_view,text="Grid",font=("HG丸ｺﾞｼｯｸM-PRO",14,"bold"),bg="white",command=lambda:self.base_line("None"))
+        self.open_menu.place(x=1160,y=840)
+        self.back_line.place(x=1400,y=840)
+        self.load_line.place(x=1320,y=840)
+        self.line_mode.place(x=130,y=840)
+        self.clear_list.place(x=1240,y=840)
+        self.point_label.place(x=20,y=840,width=100,height=37)
+
+        self.Route_view.bind("<Motion>",self.get_mouse) 
+        self.Route_view.bind('<Control-t>',self.new_track,"+")
+        self.Route_view.bind('<Control-b>',self.base_line,"+")
+
+        self.Route_view.focus_set()
+
+        Sinbol.Track_01("1RAT",550,550)
+        Sinbol.Sinbol_02(750,450,"None")
+
+    def new_track(self,event):
+
+        self.entry_box(event,"軌道回路名 入力","軌道回路 名称")
+
+    def entry_box(self,event,name,meg):
+
+        self.dialog=tk.Toplevel(self)
+        self.dialog.title(name)
+        self.dialog.geometry("300x50+750+450")
+        self.dialog.resizable(width=False, height=False)                                                            
+        self.dialog.grab_set()
+        self.dialog.focus_set()
+        self.dialog.transient(self.master)
+        self.entry_box=ttk.Entry(self.dialog,width=30)
+        entry_label=ttk.Label(self.dialog,text=meg)
+        self.entry_box.place(x=((len(meg)-1)*15+5),y=15)
+        entry_label.place(x=10,y=15)
+        self.entry_box.bind('<Return>',self.track_add)
+        self.entry_box.focus_get()
+
+    def track_add(self,event):
+
+        track=self.entry_box.get()
+        if track=="":
+            return
+        self.mode="make track"
+        self.dialog.destroy()
+
+    def switch_isSnap(self):
+
+        pass
+    
+    def get_mouse(self,event):
+        
+        self.mouseX,self.mouseY=event.x,event.y
+        self.point_var.set("x:"+str(int(event.x/20)).zfill(2)+" y:"+str(int(event.y/20)).zfill(2))
+    
+    def base_line(self,event):
+
+        i,height_dig,width_dig=0,50,100
+        if self.grid_modo==True:
+            while i<1500:
+                self.Route_view.create_line(i+width_dig,0,i+width_dig,900,tags="base_line",fill="white")
+                i=i+width_dig
+            i=0
+            while i<900:
+                self.Route_view.create_line(0,i+height_dig,1500,i+height_dig,tags="base_line",fill="white")
+                i=i+height_dig
+        else :
+            self.Route_view.delete("base_line")
+        self.grid_modo=not self.grid_modo
+
+
+
+if __name__=="__main__":
+
+    root=tk.Tk()
+    app=Application(master=root)
+    app.mainloop()
---
+++ sinbol.py
@@ -0,0 +1,201 @@
+#ドラッグオブジェクト　上位クラス
+#from pyxel import width
+#
+
+class Canvas_object:
+
+    canvas=None
+
+    def drag_start(self,event):
+
+        self.x,self.y=event.x,event.y
+        #self.load_csv(event)
+
+    def dragging(self,event):
+
+        x1,y1=(int(event.x/10)*10),(int(event.y/10)*10)
+        for i in range(len(self.parts_list)):
+            Canvas_object.canvas.lift(self.name+self.parts_list[i])
+            Canvas_object.canvas.move(self.name+self.parts_list[i],x1-self.x,y1-self.y)
+        self.x,self.y=x1,y1 
+        #self.load_csv(event)                     
+
+    def event_bind(self,name,parts_list,x,y,flg):
+
+        self.name,self.parts_list,self.x,self.y,self.flg=name,parts_list,x,y,flg
+        for i in range(0,len(self.parts_list)):
+            Canvas_object.canvas.tag_bind(self.name+self.parts_list[i],"<Button-1>",self.drag_start)
+            Canvas_object.canvas.tag_bind(self.name+self.parts_list[i],"<Button1-Motion>",self.dragging)
+            if self.flg==True:
+                Canvas_object.canvas.tag_bind(self.name+self.parts_list[i],"<Double-1>",self.switch_draw)
+
+#着点有/発点両方向
+class Track_01(Canvas_object):
+
+    def __init__(self,name,x,y,lengh):
+
+        self.name,self.x,self.y=name,x,y
+        self.EndPointOffset,self.Track_length,self.size=50,300,20
+        Canvas_object.canvas.create_line(self.x,self.y,self.x+self.Track_length,self.y,fill="white",width=2,tags=self.name+"A")
+        Canvas_object.canvas.create_polygon(
+            self.x+self.EndPointOffset,self.y,
+            self.x+self.EndPointOffset+self.size,self.y+int(self.size/2),
+            self.x+self.EndPointOffset+self.size,self.y-int(self.size/2),
+            width=2,outline="white",fill="black",tags=self.name+"B"
+            )
+        Canvas_object.canvas.create_polygon(
+            self.x+self.Track_length-self.EndPointOffset+self.size,self.y,
+            self.x+self.Track_length-self.EndPointOffset,self.y+int(self.size/2),
+            self.x+self.Track_length-self.EndPointOffset,self.y-int(self.size/2),
+            width=2,outline="white",fill="black",tags=self.name+"B"
+            )
+        Canvas_object.canvas.create_oval(
+            self.x+int(self.Track_length/2)-self.size,self.y+int(self.size/2),
+            self.x+int(self.Track_length/2)+self.size,self.y-int(self.size/2),
+            width=2,outline="white",tags=self.name+"C"
+        )
+        Canvas_object.event_bind(self,self.name,["A","B","C","D"],self.x,self.y,False)
+
+#着点有/発点片方向
+class Track_02(Canvas_object):
+
+    def __init__(self):
+
+        pass
+
+#
+class Free_track(Canvas_object):
+
+    def __init__(self):
+
+        pass
+
+class Sinbol_01(Canvas_object):
+
+    def __init__(self,x,y,name):
+
+        self.x,self.y,self.name,oval_size,grund_size,heght,self.aspect=x,y,"Sinbol-01-"+name,15,10,10,True
+        Canvas_object.canvas.create_oval(
+            self.x+heght,self.y-int(oval_size/2),self.x+heght+oval_size,self.y+int(oval_size/2),fill="black",outline="white",width=2,tags=self.name+"A"    
+        )
+        Canvas_object.canvas.create_line(
+            self.x,self.y+int(grund_size/2),self.x,self.y-int(grund_size/2),fill="white",width=2,tags=self.name+"B"
+        )
+        Canvas_object.canvas.create_line(
+            self.x,self.y,self.x+heght+oval_size,self.y,fill="white",width=2,tags=self.name+"C"    
+        )
+        Canvas_object.canvas.create_line(
+            self.x+heght+int(oval_size/2)+1,self.y+int(oval_size/2),self.x+heght+int(oval_size/2)+1,self.y-int(oval_size/2),fill="white",width=2,tags=self.name+"D"    
+        )
+        Canvas_object.event_bind(self,self.name,["A","B","C","D"],self.x,self.y,True)
+
+    def switch_draw(self,event):
+
+        self.aspect,oval_size,grund_size,heght=not self.aspect,15,10,10
+        if self.aspect==True:
+            Canvas_object.canvas.coords(self.name+"A",self.x+heght,self.y-int(oval_size/2),self.x+heght+oval_size,self.y+int(oval_size/2))
+            Canvas_object.canvas.coords(self.name+"B",self.x,self.y+int(grund_size/2),self.x,self.y-int(grund_size/2))
+            Canvas_object.canvas.coords(self.name+"C",self.x,self.y,self.x+heght+oval_size,self.y)
+            Canvas_object.canvas.coords(self.name+"D",self.x+heght+int(oval_size/2)+1,self.y+int(oval_size/2),self.x+heght+int(oval_size/2)+1,self.y-int(oval_size/2))
+        else:
+            Canvas_object.canvas.coords(self.name+"A",self.x-heght,self.y+int(oval_size/2),self.x-heght-oval_size,self.y-int(oval_size/2))
+            Canvas_object.canvas.coords(self.name+"B",self.x,self.y-int(grund_size/2),self.x,self.y+int(grund_size/2))
+            Canvas_object.canvas.coords(self.name+"C",self.x,self.y,self.x-heght-oval_size,self.y)
+            Canvas_object.canvas.coords(self.name+"D",self.x-heght-int(oval_size/2),self.y-int(oval_size/2),self.x-heght-int(oval_size/2),self.y+int(oval_size/2))
+
+#車両停止位置
+class Sinbol_02(Canvas_object):
+
+    def __init__(self,x,y,name):
+
+        self.x,self.y,self.name,rect_size,grund_size,heght,self.aspect=x,y,"Sinbol-02"+name,12,10,10,True
+        Canvas_object.canvas.create_line(
+            self.x,self.y+int(grund_size/2),self.x,self.y-int(grund_size/2),fill="white",width=2,tags=self.name+"A"
+        )
+        Canvas_object.canvas.create_rectangle(
+            self.x+heght,self.y-int(rect_size/2),self.x+heght+rect_size,self.y+int(rect_size/2),outline="white",fill="black",width=2,tags=self.name+"B"
+        )
+        Canvas_object.canvas.create_line(
+            self.x,self.y,self.x+heght+rect_size,self.y,fill="white",width=2,tags=self.name+"C"    
+        )
+        Canvas_object.canvas.create_line(
+            self.x+heght+int(rect_size/2),self.y+int(rect_size/2),self.x+heght+int(rect_size/2),self.y-int(rect_size/2),fill="white",width=2,tags=self.name+"D"    
+        )
+        Canvas_object.event_bind(self,self.name,["A","B","C","D"],self.x,self.y,True)
+
+    def switch_draw(self,event):
+
+        rect_size,grund_size,heght=12,10,10
+        self.aspect=not self.aspect
+        if self.aspect==True:
+            Canvas_object.canvas.coords(self.name+"A",self.x,self.y+int(grund_size/2),self.x,self.y-int(grund_size/2))
+            Canvas_object.canvas.coords(self.name+"B",self.x+heght,self.y-int(rect_size/2),self.x+heght+rect_size,self.y+int(rect_size/2))
+            Canvas_object.canvas.coords(self.name+"C",self.x,self.y,self.x+heght+rect_size,self.y)
+            Canvas_object.canvas.coords(self.name+"D",self.x+heght+int(rect_size/2),self.y+int(rect_size/2),self.x+heght+int(rect_size/2),self.y-int(rect_size/2))
+        else:
+            Canvas_object.canvas.coords(self.name+"A",self.x,self.y+int(grund_size/2),self.x,self.y-int(grund_size/2))
+            Canvas_object.canvas.coords(self.name+"B",self.x-heght,self.y+int(rect_size/2),self.x-heght-rect_size,self.y-int(rect_size/2))
+            Canvas_object.canvas.coords(self.name+"C",self.x,self.y,self.x-heght-rect_size,self.y)
+            Canvas_object.canvas.coords(self.name+"D",self.x-heght-int(rect_size/2),self.y-int(rect_size/2),self.x-heght-int(rect_size/2),self.y+int(rect_size/2))
+
+#車止標識
+class Sinbol_03(Canvas_object):
+
+    def __init__(self,x,y,name):
+
+        self.x,self.y,self.name,rect_size=x,y,"Sinbol-03"+name,20
+        Canvas_object.canvas.create_rectangle(
+            self.x,self.y,self.x+rect_size,self.y+rect_size,outline="white",fill="black",width=2,tags=self.name+"A"
+        )
+        Canvas_object.canvas.create_line(
+            self.x,self.y,self.x+rect_size,self.y+rect_size,fill="white",width=2,tags=self.name+"B"    
+        )
+        Canvas_object.canvas.create_line(
+            self.x,self.y+rect_size,self.x+rect_size,self.y,fill="white",width=2,tags=self.name+"C"    
+        )
+        Canvas_object.event_bind(self,self.name,["A","B","C"],self.x,self.y,False)
+
+#車両接触限界標識
+class Sinbol_04(Canvas_object):
+
+    def __init__(self,x,y,name):
+
+        self.x,self.y,self.name,size=x,y,"Sinbol-04"+name,8
+        Canvas_object.canvas.create_line(
+            self.x,self.y,self.x+size,self.y+size,fill="white",width=2,tags=self.name+"A"    
+        )
+        Canvas_object.canvas.create_line(
+            self.x,self.y+size,self.x+size,self.y,fill="white",width=2,tags=self.name+"B"    
+        )
+        Canvas_object.event_bind(self,self.name,["A","B"],self.x,self.y,False)
+ 
+#信号扱所
+class Sinbol_05(Canvas_object):
+
+    def __init__(self,x,y,name):
+
+        self.x,self.y,self.name,heght,width=x,y,"Sinbol-05"+name,35,60
+        Canvas_object.canvas.create_rectangle(
+            self.x,self.y,self.x+width,self.y+heght,outline="white",fill="black",width=2,tags=self.name+"A"
+        )
+        Canvas_object.canvas.create_rectangle(
+            self.x+5,self.y+5,self.x+width-5,self.y+int(heght/3),outline="white",fill="white",width=2,tags=self.name+"A"
+        )
+        Canvas_object.canvas.create_oval(
+            self.x+int(width/2)-5,self.y+20,self.x+int(width/2)+5,self.y+30,fill="white",width=2,tags=self.name+"C"    
+        )
+        Canvas_object.event_bind(self,self.name,["A","B","C"],self.x,self.y,False)
+
+#軌道回路区分
+class Sinbol_06(Canvas_object):
+
+    def __init__(self,x,y,name):
+
+        self.x,self.y,self.name,size=x,y,"Sinbol-06"+name,15
+        Canvas_object.canvas.create_line(
+            self.x,self.y,self.x,self.y+size,fill="white",width=2,tags=self.name+"A"    
+        )
+        Canvas_object.canvas.create_line(
+            self.x+5,self.y,self.x+5,self.y+size,fill="white",width=2,tags=self.name+"B"    
+        )
+        Canvas_object.event_bind(self,self.name,["A","B"],self.x,self.y,False)
