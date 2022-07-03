de2c11c2b9db245cc4224a9f29fdc404baec7136
Yuzu-ponzu <konamikann.exe57293@gmail.com>
2022-07-03 14:04:38 +0900

    着発線　発点/回路区分　追加

--- sinbol.py
+++ sinbol.py
@@ -30,36 +30,47 @@ class Canvas_object:
                 Canvas_object.canvas.tag_bind(self.name+self.parts_list[i],"<Double-1>",self.switch_draw)
 
 #着点有/発点両方向
-class Track_01(Canvas_object):
+class Track(Canvas_object):
 
-    def __init__(self,name,x,y,length,left,right,button):
+    def __init__(self,name,x,y,length,left,right,button,track,text_mid,section):
 
-        self.name,self.x,self.y,self.left,self.right,self.parts=name,x,y,left,right,["A"]
-        self.EndPointOffset,self.Track_length,self.size,self.button=50,length,20,button
+        self.name,self.x,self.y,self.left,self.right,self.parts,self.section=name,x,y,left,right,["A"],section
+        self.EndPointOffset,self.Track_length,self.size,self.button,self.track,self.text_mid=50,length,20,button,track,text_mid
         Canvas_object.canvas.create_line(self.x,self.y,self.x+self.Track_length,self.y,fill="white",width=2,tags=self.name+"A")
-        if self.left==True:
-            Canvas_object.canvas.create_polygon(
-                self.x+self.EndPointOffset,self.y,
-                self.x+self.EndPointOffset+self.size,self.y+int(self.size/2),
-                self.x+self.EndPointOffset+self.size,self.y-int(self.size/2),
-                width=2,outline="white",fill="black",tags=self.name+"B"
+        if self.section==True:
+            self.parts.append("G")
+            self.parts.append("H")
+            Canvas_object.canvas.create_line(self.x,self.y-5,self.x,self.y+5,fill="white",width=2,tags=self.name+"G")
+            Canvas_object.canvas.create_line(self.x+self.Track_length,self.y-5,self.x+self.Track_length,self.y+5,fill="white",width=2,tags=self.name+"H")
+        if self.track==False:
+            if self.left==True:
+                Canvas_object.canvas.create_polygon(
+                    self.x+self.EndPointOffset,self.y,
+                    self.x+self.EndPointOffset+self.size,self.y+int(self.size/2),
+                    self.x+self.EndPointOffset+self.size,self.y-int(self.size/2),
+                    width=2,outline="white",fill="black",tags=self.name+"B"
+                    )
+                self.parts.append("B")
+            if self.right==True:
+                Canvas_object.canvas.create_polygon(
+                    self.x+self.Track_length-self.EndPointOffset+self.size,self.y,
+                    self.x+self.Track_length-self.EndPointOffset,self.y+int(self.size/2),
+                    self.x+self.Track_length-self.EndPointOffset,self.y-int(self.size/2),
+                    width=2,outline="white",fill="black",tags=self.name+"C"
+                    )
+                self.parts.append("C")
+            if self.button!=False and self.track==False:
+                Canvas_object.canvas.create_oval(
+                    self.x+int(self.Track_length/2)-int(self.size/2),self.y-int(self.size/2),
+                    self.x+int(self.Track_length/2)+int(self.size/2),self.y+int(self.size/2),
+                    width=2,outline="white",tags=self.name+"D",fill="black"
                 )
-            self.parts.append("B")
-        if self.right==True:
-            Canvas_object.canvas.create_polygon(
-                self.x+self.Track_length-self.EndPointOffset+self.size,self.y,
-                self.x+self.Track_length-self.EndPointOffset,self.y+int(self.size/2),
-                self.x+self.Track_length-self.EndPointOffset,self.y-int(self.size/2),
-                width=2,outline="white",fill="black",tags=self.name+"C"
-                )
-            self.parts.append("C")
-        if self.button!=False:
-            Canvas_object.canvas.create_oval(
-                self.x+int(self.Track_length/2)-int(self.size/2),self.y-int(self.size/2),
-                self.x+int(self.Track_length/2)+int(self.size/2),self.y+int(self.size/2),
-                width=2,outline="white",tags=self.name+"D",fill="black"
-            )
-            self.parts.append("D")
+                self.parts.append("D")
+        else:
+            self.parts.append("E")
+            self.parts.append("F")
+            Canvas_object.canvas.create_rectangle(self.x+int(self.Track_length/2)-self.text_mid,self.y-5,self.x+int(self.Track_length/2)+self.text_mid,self.y+5,fill="black",tags=self.name+"E")
+            Canvas_object.canvas.create_text(self.x+int(self.Track_length/2),self.y,text="("+self.name+")",fill="white",font=("HG丸ｺﾞｼｯｸM-PRO",14,"bold"),tags=self.name+"F")
         Canvas_object.event_bind(self,self.name,self.parts,self.x,self.y,False)
 
 #着点有/発点片方向
@@ -69,13 +80,17 @@ class Track_02(Canvas_object):
 
         self.name,self.line=name,line
         Canvas_object.canvas.create_line(self.line,fill="white",width=2,tags=self.name+"A")
-        Canvas_object.event_bind(self,self.name,["A"],self.x,self.y,False)
+        #Canvas_object.event_bind(self,self.name,["A"],self.x,self.y,False)
 #
 class Free_track(Canvas_object):
 
-    def __init__(self):
+    def __init__(self,name,track_x,track_y):
 
-        pass
+        self.name,self.track_x,self.track_y=name,track_x,track_y
+        for i in range(1,len(self.track_x)):
+            print(self.track_x[i],self.track_y[i])
+            Canvas_object.canvas.create_line(self.track_x[i-1],self.track_y[i-1],self.track_x[i],self.track_y[i],width=2,fill="white",tags=self.name+"A")
+        Canvas_object.event_bind(self,self.name,["A"],self.track_x[0],self.track_y[0],False)
 
 class Sinbol_01(Canvas_object):
 
